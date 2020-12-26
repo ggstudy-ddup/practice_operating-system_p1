020fe821bb6593385268b8b39b8a272b4fe04e19
Joss Lei <825996020@qq.com>
2020-12-26 22:54:11 +0800

    kernel with simple printf

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,6 +1,15 @@
+#include "define.h"
 
-void kernel_main()
+static word *video_mem_ptr = (word*) 0x68000;
+void printf(char *str)
 {
+    for (dword i = 0; str[i]; i++)
+        video_mem_ptr[i] = (0xFF00 & video_mem_ptr[i]) | str[i];
+}
+
+void kernel_main(void *multiboot_struct, dword magic)
+{
+    printf("Hello world!");
 	while(1);
 }
 
