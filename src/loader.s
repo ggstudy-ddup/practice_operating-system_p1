b8310a1ecd1c3386405835ded323ebac6962858b
Joss Lei <825996020@qq.com>
2020-12-26 22:54:46 +0800

    Bootable with Grub, gives the multiboot structure as parameter to the kernel

--- src/loader.s
+++ src/loader.s
@@ -1,9 +1,20 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
 .section .text
 .extern kernel_main
 .global loader
 
 loader:
 	mov $kernel_stack, %esp
+    pushl %eax
+    pushl %ebx
 	call kernel_main
 loop:
     cli
