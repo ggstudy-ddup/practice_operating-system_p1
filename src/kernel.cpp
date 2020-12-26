8e05470834944c9367a3d1f6e11f399d6ef4dc0e
Joss Lei <825996020@qq.com>
2020-12-26 23:11:51 +0800

    Change the kernel_main into a C function

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -7,7 +7,7 @@ void printf(char *str)
         video_mem_ptr[i] = (0xFF00 & video_mem_ptr[i]) | str[i];
 }
 
-void kernel_main(void *multiboot_struct, dword magic)
+extern "C" void kernel_main(void *multiboot_struct, dword magic)
 {
     printf("Hello world!");
 	while(1);
