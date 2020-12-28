c8e6471034e0b61369c76b20afb64dbb5aa3f00b
Joss Lei <825996020@qq.com>
2020-12-28 14:46:13 +0800

    Some common defines

---
+++ src/include/define.h
@@ -0,0 +1,16 @@
+#define M32 1
+
+#ifdef M32
+
+/* define with bits */
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+/* define with its name */
+typedef u8  byte;
+typedef u16 word;
+typedef u32 dword;
+
+#endif
+
