5c2bf7d1211ae6b155d5e832204aea30864964e5
Joss <825996020@qq.com>
2021-01-08 19:11:19 +0800

    Some typedef

--- src/include/define.h
+++ src/include/define.h
@@ -2,15 +2,22 @@
 
 #ifdef M32
 
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+
 /* define with bits */
 typedef unsigned char  u8;
 typedef unsigned short u16;
 typedef unsigned int   u32;
 
 /* define with its name */
-typedef u8  byte;
-typedef u16 word;
-typedef u32 dword;
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
+
+/* other types */
+typedef unsigned int size_t;
 
 #endif
 
