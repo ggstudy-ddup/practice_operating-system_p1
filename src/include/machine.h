ab18c8da14144f2e6bc49b03b67a8bef07bfb564
Joss <825996020@qq.com>
2021-01-11 16:28:20 +0800

    Headers

---
+++ src/include/machine.h
@@ -0,0 +1,13 @@
+#include <define.h>
+
+#ifndef __INCLUDE_MACHINE_H__
+#define __INCLUDE_MACHINE_H__
+
+/* write a byte out, to the port */
+inline void outb(word port, byte val);
+
+/* read a byte in, from the port */
+inline byte inb(word port);
+
+#endif  /* !__INCLUDE_MACHINE_H__ */
+
