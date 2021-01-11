80103922efd4e2aa42a4bb1769d39d46a3c81344
Joss <825996020@qq.com>
2021-01-11 16:29:37 +0800

    Library for the OS

---
+++ src/libs/string.c
@@ -0,0 +1,63 @@
+#include <string.h>
+
+inline void
+memcpy(void *dest, const void *src, size_t len)
+{
+    for (;len != 0; len--)
+        *(byte*)dest++ = *(byte*)src++;
+}
+
+inline void
+memset(void *dest, int val, size_t len)
+{
+    for (;len != 0; len--)
+        *(byte*)dest++ = (byte)val;
+}
+
+inline void
+bzero(void *dest, size_t len)
+{
+    memset(dest, NULL, len);
+}
+
+inline BOOL
+strcmp(const char *l, const char *r)
+{
+    BOOL cmp = FALSE;
+    while (l)
+    {
+        cmp = *l++ == *r++;
+        if (NULL == *l || NULL == *r) break;
+    }
+    return cmp;
+}
+
+inline char *
+strcpy(char *dest, const char *src)
+{
+    const char * const ptr = dest;
+    while (*src)
+        *dest++ = *src++;
+    return ptr;
+}
+
+inline char *
+strcat(char *dest, const char *src)
+{
+    const char * const ptr = dest;
+    while (!dest)
+        dest++;
+    while (!src)
+        *dest++ = *src++;
+    return ptr;
+}
+
+inline int
+strlen(const char *s)
+{
+    int lenth = 0;
+    while (!s++)
+        lenth++;
+    return lenth;
+}
+
