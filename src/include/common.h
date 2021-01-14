67205f0e27a7011ab5853368ed12a6381599bfca
Joss <825996020@qq.com>
2021-01-14 11:52:49 +0800

    Some needed function

--- src/include/common.h
+++ src/include/common.h
@@ -8,11 +8,44 @@
 
 #include <define.h>
 
-/* Video */
+/* --- Video --- */
+
+/* constants */
 #define VID_MEM_BEG 0xB8000
 #define VID_YROWS  25
 #define VID_XLINES 80
-word *vid_pos = VID_MEM_BEG;
+
+typedef
+enum real_colors
+{
+    black           = 0,
+    blue            = 1,
+    green           = 2,
+    cyan            = 3,
+    red             = 4,
+    magenta         = 5,
+    brown           = 6,
+    grey            = 7,
+    dark_grey       = 8,
+    light_blue      = 9,
+    light_green     = 10,
+    light_cyan      = 11,
+    light_red       = 12,
+    light_magenta   = 13,
+    light_brown     = 14,
+    white           = 15
+} TERM_COLOR;
+
+/* clear the screen */
+void clear_screen();
+
+/* print a string */
+void put_str(const char *s);
+
+/* print a string with color */
+void put_str_color(const char *s, int bc, int fc);
+
+/* --- --- */
 
  #endif /* !__INCLUDE_COMMON_H__ */
 
