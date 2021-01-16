5b54d21c3cf4036f2df1205f9fe13c4c7747e867
Joss <825996020@qq.com>
2021-01-16 22:07:47 +0800

    String display function finished

--- src/include/common.h
+++ src/include/common.h
@@ -7,37 +7,48 @@
 #define __INCLUDE_COMMON_H__
 
 #include <define.h>
+#include <bottom.h>
+#include <string.h>
 
 /* --- Video --- */
 
 /* constants */
 #define VID_MEM_BEG (word*)(0xB8000)
-#define VID_YROWS  (25)
-#define VID_XLINES (80)
+#define VID_YMAX  (25)
+#define VID_XMAX (80)
 
 typedef
-enum real_colors
+enum console_real_colors
 {
-    black           = 0,
-    blue            = 1,
-    green           = 2,
-    cyan            = 3,
-    red             = 4,
-    magenta         = 5,
-    brown           = 6,
-    grey            = 7,
-    dark_grey       = 8,
-    light_blue      = 9,
-    light_green     = 10,
-    light_cyan      = 11,
-    light_red       = 12,
-    light_magenta   = 13,
-    light_brown     = 14,
-    white           = 15
-} TERM_COLOR;
+    rc_black           = 0,
+    rc_blue            = 1,
+    rc_green           = 2,
+    rc_cyan            = 3,
+    rc_red             = 4,
+    rc_magenta         = 5,
+    rc_brown           = 6,
+    rc_grey            = 7,
+    rc_dark_grey       = 8,
+    rc_light_blue      = 9,
+    rc_light_green     = 10,
+    rc_light_cyan      = 11,
+    rc_light_red       = 12,
+    rc_light_magenta   = 13,
+    rc_light_brown     = 14,
+    rc_white           = 15
+} termrc;
+
+/* set cursor position */
+void set_cursor_pos();
+
+/* scroll a line up */
+void scroll_up();
 
 /* clear the screen */
 void clear_screen();
+
+/* display a char */
+void put_char(char c, int bg, int fc);
 
 /* print a string */
 void put_str(const char *s);
--- src/libs/common.c
+++ src/libs/common.c
@@ -1,36 +1,127 @@
 #include <common.h>
 
-/* video ptr position */
-static word *vid_pos = VID_MEM_BEG;
-
 /* cursor position */
 static byte cursor_x = 0;
 static byte cursor_y = 0;
 
 void
+set_cursor_pos()
+{
+    /* position in the video memory */
+    word cursor_vidmem_pos = cursor_y * VID_XMAX + cursor_x;
+
+    /* addr port:
+     *  0x3D4
+     * val port:
+     *  0x3D5
+     */
+    /* cursor position low-register:
+     *     15
+     * cursor position high-register:
+     *     14
+     */
+
+    /* cursor position lower byte */
+    outb(0x3D4, 15);
+    outb(0x3D5, cursor_vidmem_pos);
+    /* cursor position higher byte */
+    outb(0x3D4, 14);
+    outb(0x3D5, cursor_vidmem_pos >> 8);
+}
+
+void
+scroll_up()
+{
+    /* substitute the lines by their next lines */
+    for (int i = 0; i < VID_YMAX - 1 /* without the last line */; i++)
+    {
+        memcpy(VID_MEM_BEG +                    /* Starting addr */
+                    i                           /* line number */
+                    * sizeof(word) * VID_XMAX,  /* offset bytes per line */
+               VID_MEM_BEG + (i + 1) * sizeof(word) * VID_XMAX,
+               sizeof(word) * VID_XMAX);        /* line len */
+    }
+    /* clear the last line */
+    bzero(VID_MEM_BEG +                 /* Starting addr */
+            (VID_YMAX - 1)              /* the last line */
+            * sizeof(word) * VID_XMAX,  /* offset bytes per line */
+          sizeof(word) * VID_XMAX);     /* line len */
+    /* set the cursor */
+    if (cursor_y)   /* cursor_y != 0 */
+        cursor_y--; /* keep the cursor in the same ouput line */
+}
+
+void
 clear_screen()
 {
+    /* video ptr position */
+    word *vid_pos = VID_MEM_BEG;
+
+    /* blank */
     byte attribute_byte = (0 << 4) | (15 & 0x0F);
     word blank = ' ' | (attribute_byte << 8);
-    for (int i = 0; i < VID_YROWS * VID_XLINES; i++)
+    for (int i = 0; i < VID_YMAX * VID_XMAX; i++)
         *vid_pos++ = blank;
-    vid_pos = VID_MEM_BEG;
+
+    /* set cursor to the very first position */
+    cursor_x = cursor_y = 0;
+    set_cursor_pos();
+}
+
+void
+put_char(char c, int bg, int fg)
+{
+    word attribute = (word)(((byte)bg << 4) | ((byte)fg & 0x0F)) << 8;
+    switch (c)
+    {
+    /* backspace */
+    case 0x08:
+        if (cursor_x) cursor_x--;
+        break;
+    /* tab */
+    case 0x09:
+        cursor_x += 8 - (cursor_x % 8);
+        break;
+    /* \r */
+    case '\r':
+        cursor_x = 0;
+        break;
+    /* linebreak */
+    case '\n':
+        cursor_x = 0;
+        cursor_y++;
+        break;
+    /* normal char */
+    case (0x20)...(0x7E):
+        *(word*)(VID_MEM_BEG + cursor_y * VID_XMAX + cursor_x) = c | attribute;
+        cursor_x++;
+        break;
+    default:
+        break;
+    }
+    if (VID_XMAX <= cursor_x)
+    {
+        cursor_x = 0;
+        cursor_y++;
+    }
+    if (VID_YMAX <= cursor_y)
+        scroll_up();
+
+    /* refresh the cursor position */
+    set_cursor_pos();
 }
 
 void
 put_str(const char *s)
 {
-    byte attribute_byte = (0 << 4) | (15 & 0x0F);
-    for (; *s; s++)
-    {
-        switch (*s)
-        {
-        default:
-            *vid_pos++ = (word)(*s | (attribute_byte << 8));
-        }
-    }
+    while (*s)
+        put_char(*s++, rc_black, rc_white);
 }
 
 void
-put_str_color(const char *s, int bc, int fc);
+put_str_color(const char *s, int bc, int fc)
+{
+    while (*s)
+        put_char(*s++, bc, fc);
+}
 
