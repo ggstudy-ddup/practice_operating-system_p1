8d591819637c9e03447dc78ed7cd9c417955f28c
Joss <825996020@qq.com>
2021-01-16 11:20:27 +0800

    Hello World

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,7 +3,11 @@
 void kernel_main(void *multiboot_struct, dword magic)
 {
     clear_screen();
+    put_str("Hello world, new OS!");
+    
+    /* Sleeping */
+    asm volatile ("cli");
     while (TRUE)
-        asm volatile("hlt");
+        asm volatile ("hlt");
 }
 
