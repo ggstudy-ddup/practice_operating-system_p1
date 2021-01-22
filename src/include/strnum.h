607a1c53860963fa3768d81b779cef8c1909500a
Joss <825996020@qq.com>
2021-01-22 16:52:50 +0800

    Available in Intel x86

---
+++ src/include/strnum.h
@@ -0,0 +1,12 @@
+#ifndef __INCLUDE_STRNUM_H__
+#define __INCLUDE_STRNUM_H__
+
+#include <define.h>
+
+/* int to ascii */
+char * itoa(int i, char * buf);
+
+/* long to ascii */
+// char * ltoa(long i, 
+
+#endif /* !__INCLUDE_STRNUM_H__ */
