cf27acacc6b6c85f83e084fbbe47b5df55d62917
Joss <825996020@qq.com>
2021-01-23 18:49:51 +0800

    Function sprintf() implemented

--- src/include/io/screen.h
+++ src/include/io/screen.h
@@ -1,4 +1,10 @@
 #ifndef __INCLUDE_IO_SCREEN_H__
 #define __INCLUDE_IO_SCREEN_H__
 
+#include <types.h>
+
+int sprintf(char * buf, char * format, ...);
+
+int printk(char * format, ...);
+
 #endif /* !__INCLUDE_IO_SCREEN_H__ */
---
+++ src/io/screen.c
@@ -0,0 +1,30 @@
+#include <io/screen.h>
+
+int
+my_sprintf(char * buf, char * format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    int count = 0;
+    while (*format)
+    {
+        if ('%'  == *format)
+        {
+            ++format;
+            switch (*format)
+            {
+            case 'x':
+                count += itoa(va_arg(list, int), buf + count);
+                break;
+            default:
+                break;
+            }
+        }
+        else
+            *(char*)(buf + count++) = *format;
+        ++format;
+    }
+    va_end(list);
+    return count;
+}
+
