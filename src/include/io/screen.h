b84a1bd5b7ec7afa098d95bb52c62d6188f3c057
Joss <825996020@qq.com>
2021-01-26 00:01:25 +0800

    Normalization

--- src/include/io/screen.h
+++ src/include/io/screen.h
@@ -4,8 +4,23 @@
 #include <types.h>
 #include <strnum.h>
 
-int sprintf(char * buf, char * format, ...);
+#define PRINTK_BUF_SIZE ((int)(VID_XMAX * VID_YMAX))
 
-int printk(char * format, ...);
+/*
+ * Do the process of string formatting
+ * @param buf the output buffer
+ * @param format the format str
+ * @param list the variable parameter list
+ * @ret the length of output str
+ */
+int sprintf(char * buf, cosnt char * format, va_list list);
+
+/*
+ * Print function for kernel
+ * @param format the format str
+ * @param ... 
+ * @ret the length of print str
+ */
+int printk(const char * format, ...);
 
 #endif /* !__INCLUDE_IO_SCREEN_H__ */
